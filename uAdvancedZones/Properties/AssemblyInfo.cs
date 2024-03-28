163dc69f92b53af3ede4f51722ca11206e2ee783
Luis Tanese <luistanese@gmail.com>
2024-03-28 11:10:47 -0300

    Add files via upload

---
+++ uAdvancedZones/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("uAdvancedZones")]
+[assembly: AssemblyDescription("You create a zone using the plugin AdvancedZones for rocketMod, and when a player enters or leaves the zone, it will trigger the event.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AV Plugins")]
+[assembly: AssemblyProduct("uAdvancedZones")]
+[assembly: AssemblyCopyright("Copyright © AV Plugins 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f0762b2b-6359-4b36-95cc-6bf1ed96dc08")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
